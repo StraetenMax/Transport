d61d543bee6fb7061ca0f52713f610b89225ea6a
StraetenMax <maxstraeten@yahoo.com>
2017-05-25 18:00:00 +0200

    class Bateau propriétés constructeurs

---
+++ src/voyage/Bateau.java
@@ -0,0 +1,20 @@
+package voyage;
+
+public class Bateau {
+	//propriétés
+	private int nbPassagers;
+	private String couleur;
+	private String energie;
+	private int puissance;
+	private int vitesseMax;
+	
+	//constructeurs vide et initialisé
+	public Bateau(){
+		nbPassagers = 0;
+		couleur = "";
+		energie = "";
+		puissance = 0;
+		vitesseMax = 0;
+	}
+
+}
