fa1b4f85a4cbfd6390e7259219a5840c240c04a6
StraetenMax <maxstraeten@yahoo.com>
2017-05-28 22:31:17 +0200

    Exception

---
+++ src/voyage/NbPassagersException.java
@@ -0,0 +1,8 @@
+package voyage;
+
+public class NbPassagersException extends Exception{
+	public NbPassagersException(){
+		System.out.println("Instanciation de Bateau avec nbPassagers negatif");
+	}
+
+}
